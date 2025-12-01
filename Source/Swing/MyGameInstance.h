3b3330743918d075ebf4bd6f945f06772422c599
student <>
2025-12-01 19:22:51 +0900

    青田_値の変更。インスタタンスクラスの追加。

---
+++ Source/Swing/MyGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWING_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	APawn* m_pPlayer;
+};
